9c89a4898dbd6170aa3af7bdecc960d66be880ce
houzhiqiang <zhq.hou@muheda.com>
2019-09-04 09:50:58 +0800

    测试demo移动

---
+++ src/views/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    <SimpleStorage/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SimpleStorage from '@/components/SimpleStorage.vue'
+
+export default {
+  name: 'test',
+  components: {
+    SimpleStorage
+  }
+}
+</script>
